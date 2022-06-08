42c30bfe4d4389690e37f628c2f62cb064d35286
Arthur Candido <103594555+oArthurCandido@users.noreply.github.com>
2022-06-08 16:39:22 -0300

    commiting in th end of class 18, everything ok

--- index.js
+++ index.js
@@ -5,6 +5,18 @@ const mysql = require('mysql2');
 const app = express();
 //definição da porta
 const port = 3000;
+
+//Abrindo conexão com base de dados
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '123456',
+  database: 'sistema_noticias'
+});
+
+connection.connect();
+
 // Serviço de Hello World!
 app.get('/', (req, res) => {
   res.send('Hello World!');
@@ -12,13 +24,33 @@ app.get('/', (req, res) => {
 
 //Serviço de busca de categorias
 app.get('/news-api/v1/categorias', (req, res) => {
-  res.send([
-    {
-      Name: 'Arthur',
-      idade: 32
+  //Busca categorias
+
+  connection.query(
+    'SELECT id, nome FROM sistema_noticias.categoria',
+    (err, rows, fields) => {
+      if (err) throw err;
+
+      res.send(rows);
     }
-  ]);
+  );
 });
+
+//Serviço de busca de noticias
+app.get('/news-api/v1/categorias/:categoriaId/noticias', (req, res) => {
+  res.send(req.params.categoriaId);
+  //Busca categorias
+
+  // connection.query(
+  //   'SELECT id, nome FROM sistema_noticias.categoria',
+  //   (err, rows, fields) => {
+  //     if (err) throw err;
+
+  //     res.send(rows);
+  //   }
+  // );
+});
+
 //Subindo o servidor node
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`);
